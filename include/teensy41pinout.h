4d55f2b1ca508d78ead77666eaecaa2f84629620
Michael Skiles <mjrskiles@gmail.com>
2021-08-10 10:08:30 -0500

    added pinout

---
+++ include/teensy41pinout.h
@@ -0,0 +1,21 @@
+//
+// Created by Michael Skiles on 8/9/21.
+//
+
+#ifndef SYNTH_TEENSY41PINOUT_H
+#define SYNTH_TEENSY41PINOUT_H
+
+#define BUTTON_SELECT_PIN 24
+#define BUTTON_1_PIN 25
+#define BUTTON_2_PIN 26
+#define BUTTON_3_PIN 27
+#define BUTTON_4_PIN 28
+#define BUTTON_5_PIN 29
+#define BUTTON_6_PIN 30
+#define BUTTON_7_PIN 31
+#define BUTTON_8_PIN 32
+
+#define KNOB_1_PIN 16
+#define KNOB_2_PIN 17
+
+#endif //SYNTH_TEENSY41PINOUT_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,156 @@
-#include <Arduino.h>
+// Waveform Modulation Example - Create waveforms with 
+// modulated frequency
+//
+// This example is meant to be used with 3 buttons (pin 0,
+// 1, 2) and 2 knobs (pins 16/A2, 17/A3), which are present
+// on the audio tutorial kit.
+//   https://www.pjrc.com/store/audio_tutorial_kit.html
+//
+// Use an oscilloscope to view the 2 waveforms.
+//
+// Button0 changes the waveform shape
+//
+// Knob A2 changes the amount of frequency modulation
+//
+// Knob A3 varies the shape (only for Pulse & Variable Triangle)
+//
+// This example code is in the public domain.
+
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include <Bounce.h>
+
+#include "teensy41pinout.h"
+
+AudioSynthWaveformSine   sine1;          //xy=131,97
+AudioSynthWaveformSine   sine2;          //xy=152,170
+AudioSynthWaveformModulated waveformMod1;   //xy=354,69
+AudioOutputAnalogStereo  dacs1;          //xy=490,209
+AudioOutputI2S           i2s1;           //xy=532,140
+AudioConnection          patchCord1(sine1, 0, i2s1, 1);
+AudioConnection          patchCord2(sine1, 0, dacs1, 1);
+AudioConnection          patchCord3(sine1, 0, waveformMod1, 0);
+AudioConnection          patchCord4(sine2, 0, waveformMod1, 1);
+AudioConnection          patchCord5(waveformMod1, 0, i2s1, 0);
+AudioConnection          patchCord6(waveformMod1, 0, dacs1, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=286,240
+
+Bounce buttonSelect = Bounce(BUTTON_SELECT_PIN, 15);
+Bounce button1 = Bounce(BUTTON_1_PIN, 15);
+Bounce button2 = Bounce(BUTTON_2_PIN, 15);
+Bounce button3 = Bounce(BUTTON_3_PIN, 15);
+Bounce button4 = Bounce(BUTTON_4_PIN, 15);
+Bounce button5 = Bounce(BUTTON_5_PIN, 15);
+Bounce button6 = Bounce(BUTTON_6_PIN, 15);
+Bounce button7 = Bounce(BUTTON_7_PIN, 15);
+Bounce button8 = Bounce(BUTTON_8_PIN, 15);
+
+int current_waveform=0;
+
+extern const int16_t myWaveform[256];  // defined in myWaveform.ino
+
 void setup() {
-// write your initialization code here
+    Serial.begin(9600);
+    pinMode(BUTTON_SELECT_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_1_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_2_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_3_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_4_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_5_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_6_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_7_PIN, INPUT_PULLUP);
+    pinMode(BUTTON_8_PIN, INPUT_PULLUP);
+
+
+    delay(300);
+    Serial.println("Waveform Modulation Test");
+  
+    // Audio connections require memory to work.  For more
+    // detailed information, see the MemoryAndCpuUsage example
+    AudioMemory(12);
+
+    // Comment these out if not using the audio adaptor board.
+    sgtl5000_1.enable();
+    sgtl5000_1.volume(0.8); // caution: very loud - use oscilloscope only!
+
+    // Confirgure both to use "myWaveform" for WAVEFORM_ARBITRARY
+    waveformMod1.arbitraryWaveform(myWaveform, 172.0);
+
+    // Configure for middle C note without modulation
+    waveformMod1.frequency(261.63);
+    waveformMod1.amplitude(1.0);
+    sine1.frequency(20.3); // Sine waves are low frequency oscillators (LFO)
+    sine2.frequency(1.2);
+
+    current_waveform = WAVEFORM_TRIANGLE_VARIABLE;
+    waveformMod1.begin(current_waveform);
+
+    // uncomment to try modulating phase instead of frequency
+    //waveformMod1.phaseModulation(720.0);
 }
 
 void loop() {
-// write your code here
+    // Read the buttons and knobs, scale knobs to 0-1.0
+    buttonSelect.update();
+    button1.update();
+    button2.update();
+    button3.update();
+    button4.update();
+    button5.update();
+    button6.update();
+    button7.update();
+    button8.update();
+    float knob_A2 = (float)analogRead(KNOB_1_PIN) / 1023.0;
+    float knob_A3 = (float)analogRead(KNOB_2_PIN) / 1023.0;
+
+    // use Knobsto adjust the amount of modulation
+    sine1.amplitude(knob_A2);
+    sine2.amplitude(knob_A3);
+
+    // Button select changes the waveform type
+    if (buttonSelect.fallingEdge()) {
+        switch (current_waveform) {
+            case WAVEFORM_SINE:
+                current_waveform = WAVEFORM_SAWTOOTH;
+                Serial.println("Sawtooth");
+                break;
+                case WAVEFORM_SAWTOOTH:
+                    current_waveform = WAVEFORM_SAWTOOTH_REVERSE;
+                    Serial.println("Reverse Sawtooth");
+                    break;
+                    case WAVEFORM_SAWTOOTH_REVERSE:
+                        current_waveform = WAVEFORM_SQUARE;
+                        Serial.println("Square");
+                        break;
+                        case WAVEFORM_SQUARE:
+                            current_waveform = WAVEFORM_TRIANGLE;
+                            Serial.println("Triangle");
+                            break;
+                            case WAVEFORM_TRIANGLE:
+                                current_waveform = WAVEFORM_TRIANGLE_VARIABLE;
+                                Serial.println("Variable Triangle");
+                                break;
+                                case WAVEFORM_TRIANGLE_VARIABLE:
+                                    current_waveform = WAVEFORM_ARBITRARY;
+                                    Serial.println("Arbitary Waveform");
+                                    break;
+                                    case WAVEFORM_ARBITRARY:
+                                        current_waveform = WAVEFORM_PULSE;
+                                        Serial.println("Pulse");
+                                        break;
+                                        case WAVEFORM_PULSE:
+                                            current_waveform = WAVEFORM_SAMPLE_HOLD;
+                                            Serial.println("Sample & Hold");
+                                            break;
+                                            case WAVEFORM_SAMPLE_HOLD:
+                                                current_waveform = WAVEFORM_SINE;
+                                                Serial.println("Sine");
+                                                break;
+        }
+        waveformMod1.begin(current_waveform);
+    }
+  
 }
