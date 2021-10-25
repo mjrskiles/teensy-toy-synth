13d2d05495ca266d787be8ccdc21a3cd0140054e
Michael Skiles <mjrskiles@gmail.com>
2021-10-25 14:20:43 -0500

    signal path update

--- include/synthesizer/synth_signal_chain.h
+++ include/synthesizer/synth_signal_chain.h
@@ -8,42 +8,48 @@
 #include <Audio.h>
 
 // GUItool: begin automatically generated code
-extern AudioSynthWaveformDc     dc4;            //xy=55,418.28568840026855
-extern AudioSynthWaveformDc     dc1;            //xy=56.857147216796875,188.14285278320312
-extern AudioSynthWaveformDc     dc3;            //xy=56.28572463989258,333.7142963409424
-extern AudioSynthWaveformDc     dc2;            //xy=59.0000114440918,258.7142906188965
-extern AudioSynthWaveformPWM    pwm1;           //xy=223.57144165039062,169.71429443359375
-extern AudioEffectEnvelope      pwm_env3; //xy=257,363
-extern AudioEffectEnvelope      pwm_env4; //xy=266,458
-extern AudioSynthWaveformPWM    pwm4;           //xy=273.5714340209961,406.7142958641052
-extern AudioSynthWaveformPWM    pwm2;           //xy=276.5714340209961,249.71429586410522
-extern AudioEffectEnvelope      pwm_env1;      //xy=283.28570556640625,209.71429443359375
-extern AudioSynthWaveformPWM    pwm3;           //xy=286.5714340209961,324.7142958641052
-extern AudioEffectEnvelope      pwm_env2; //xy=306,287
-extern AudioMixer4              lpf_intermediate_mixer1; //xy=529.5714111328125,159.71429443359375
-extern AudioMixer4              lpf_intermediate_mixer2; //xy=538.5714111328125,252.71429443359375
-extern AudioMixer4              lpf_intermediate_mixer3; //xy=558.5714111328125,336.71429443359375
-extern AudioMixer4              lpf_intermediate_mixer4; //xy=565.5714111328125,429.71429443359375
-extern AudioMixer4              lpf_post_mixer; //xy=1090.571434020996,261.7142958641052
-extern AudioEffectEnvelope      amp_envelope1;      //xy=1287.1428298950195,215.71429824829102
-extern AudioMixer4              post_amp_env_mixer1;         //xy=1497.1429290771484,257.1428565979004
-extern AudioEffectEnvelope      envelope2;      //xy=1499.9998931884766,351.4286241531372
-extern AudioFilterStateVariable main_lpf;       //xy=1699.8569221496582,267.42861557006836
-extern AudioOutputI2S           i2s2;           //xy=1885.8570404052734,237.42859268188477
+extern AudioSynthWaveformDc     dc1;            //xy=55,776
+extern AudioSynthWaveformDc     dc2;            //xy=64,849
+extern AudioSynthWaveformDc     dc3;            //xy=67,924
+extern AudioSynthWaveformDc     dc4;            //xy=84,1010
+extern AudioSynthWaveformPWM    pwm4;           //xy=260,997
+extern AudioSynthWaveformPWM    pwm2;           //xy=263,840
+extern AudioSynthWaveformPWM    pwm1;           //xy=273,776
+extern AudioSynthWaveformPWM    pwm3;           //xy=273,915
+extern AudioSynthWaveformDc     dc_lpf1;            //xy=403,813
+extern AudioSynthWaveformDc     dc_lpf2; //xy=403,881
+extern AudioSynthWaveformDc     dc_lpf3; //xy=405,955
+extern AudioSynthWaveformDc     dc_lpf4; //xy=406,1035
+extern AudioFilterStateVariable lpf2; //xy=578,858
+extern AudioFilterStateVariable lpf4; //xy=578,1028
+extern AudioFilterStateVariable lpf1;        //xy=579,779
+extern AudioFilterStateVariable lpf3; //xy=580,944
+extern AudioMixer4              lpf_intermediate_mixer2; //xy=814,841
+extern AudioMixer4              lpf_intermediate_mixer3; //xy=816,931
+extern AudioMixer4              lpf_intermediate_mixer1;         //xy=817,746
+extern AudioMixer4              lpf_intermediate_mixer4; //xy=820,1021
+extern AudioEffectEnvelope      pwm_env1;      //xy=1032.5,755
+extern AudioEffectEnvelope      pwm_env2; //xy=1053.75,838.75
+extern AudioEffectEnvelope      pwm_env4; //xy=1065,1020
+extern AudioEffectEnvelope      pwm_env3; //xy=1066.25,923.75
+extern AudioMixer4              lpf_post_mixer;         //xy=1270.75,888.2500305175781
+extern AudioSynthWaveformDc     dc_main_lpf;            //xy=1326.2501220703125,976
+extern AudioFilterStateVariable main_lpf;        //xy=1427.0001220703125,883.2500305175781
+extern AudioOutputI2S           i2s2; //xy=1585.08349609375,881.8611145019531
 extern AudioConnection          patchCord1;
 extern AudioConnection          patchCord2;
 extern AudioConnection          patchCord3;
 extern AudioConnection          patchCord4;
 extern AudioConnection          patchCord5;
-//extern AudioConnection          patchCord6;
+extern AudioConnection          patchCord6;
 extern AudioConnection          patchCord7;
 extern AudioConnection          patchCord8;
-//extern AudioConnection          patchCord9;
+extern AudioConnection          patchCord9;
 extern AudioConnection          patchCord10;
-//extern AudioConnection          patchCord11;
+extern AudioConnection          patchCord11;
 extern AudioConnection          patchCord12;
 extern AudioConnection          patchCord13;
-//extern AudioConnection          patchCord14;
+extern AudioConnection          patchCord14;
 extern AudioConnection          patchCord15;
 extern AudioConnection          patchCord16;
 extern AudioConnection          patchCord17;
@@ -58,7 +64,11 @@ extern AudioConnection          patchCord25;
 extern AudioConnection          patchCord26;
 extern AudioConnection          patchCord27;
 extern AudioConnection          patchCord28;
-extern AudioControlSGTL5000     sgtl5000_1;     //xy=784.2857142857142,642.8571428571428
+extern AudioConnection          patchCord29;
+extern AudioConnection          patchCord30;
+extern AudioConnection          patchCord31;
+extern AudioConnection          patchCord32;
+extern AudioControlSGTL5000     sgtl5000_1;   //xy=784.2857142857142,642.8571428571428
 // GUItool: end automatically generated code
 
 
--- src/synthesizer/synth_signal_chain.cpp
+++ src/synthesizer/synth_signal_chain.cpp
@@ -4,54 +4,73 @@
 
 #include "synthesizer/synth_signal_chain.h"
 
-AudioSynthWaveformDc     dc4;            //xy=55,418.28568840026855
-AudioSynthWaveformDc     dc1;            //xy=56.857147216796875,188.14285278320312
-AudioSynthWaveformDc     dc3;            //xy=56.28572463989258,333.7142963409424
-AudioSynthWaveformDc     dc2;            //xy=59.0000114440918,258.7142906188965
-AudioSynthWaveformPWM    pwm1;           //xy=223.57144165039062,169.71429443359375
-AudioEffectEnvelope      pwm_env3; //xy=257,363
-AudioEffectEnvelope      pwm_env4; //xy=266,458
-AudioSynthWaveformPWM    pwm4;           //xy=273.5714340209961,406.7142958641052
-AudioSynthWaveformPWM    pwm2;           //xy=276.5714340209961,249.71429586410522
-AudioEffectEnvelope      pwm_env1;      //xy=283.28570556640625,209.71429443359375
-AudioSynthWaveformPWM    pwm3;           //xy=286.5714340209961,324.7142958641052
-AudioEffectEnvelope      pwm_env2; //xy=306,287
-AudioMixer4              lpf_intermediate_mixer1; //xy=529.5714111328125,159.71429443359375
-AudioMixer4              lpf_intermediate_mixer2; //xy=538.5714111328125,252.71429443359375
-AudioMixer4              lpf_intermediate_mixer3; //xy=558.5714111328125,336.71429443359375
-AudioMixer4              lpf_intermediate_mixer4; //xy=565.5714111328125,429.71429443359375
-AudioMixer4              lpf_post_mixer; //xy=1090.571434020996,261.7142958641052
-AudioEffectEnvelope      amp_envelope1;      //xy=1287.1428298950195,215.71429824829102
-AudioMixer4              post_amp_env_mixer1;         //xy=1497.1429290771484,257.1428565979004
-AudioEffectEnvelope      envelope2;      //xy=1499.9998931884766,351.4286241531372
-AudioFilterStateVariable main_lpf;       //xy=1699.8569221496582,267.42861557006836
-AudioOutputI2S           i2s2;           //xy=1885.8570404052734,237.42859268188477
-AudioConnection          patchCord1(dc4, pwm4);
-AudioConnection          patchCord2(dc1, pwm1);
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveformDc     dc1;            //xy=55,776
+AudioSynthWaveformDc     dc2;            //xy=64,849
+AudioSynthWaveformDc     dc3;            //xy=67,924
+AudioSynthWaveformDc     dc4;            //xy=84,1010
+AudioSynthWaveformPWM    pwm4;           //xy=260,997
+AudioSynthWaveformPWM    pwm2;           //xy=263,840
+AudioSynthWaveformPWM    pwm1;           //xy=273,776
+AudioSynthWaveformPWM    pwm3;           //xy=273,915
+AudioSynthWaveformDc     dc_lpf1;            //xy=403,813
+AudioSynthWaveformDc     dc_lpf2; //xy=403,881
+AudioSynthWaveformDc     dc_lpf3; //xy=405,955
+AudioSynthWaveformDc     dc_lpf4; //xy=406,1035
+AudioFilterStateVariable lpf2; //xy=578,858
+AudioFilterStateVariable lpf4; //xy=578,1028
+AudioFilterStateVariable lpf1;        //xy=579,779
+AudioFilterStateVariable lpf3; //xy=580,944
+AudioMixer4              lpf_intermediate_mixer2; //xy=814,841
+AudioMixer4              lpf_intermediate_mixer3; //xy=816,931
+AudioMixer4              lpf_intermediate_mixer1;         //xy=817,746
+AudioMixer4              lpf_intermediate_mixer4; //xy=820,1021
+AudioEffectEnvelope      pwm_env1;      //xy=1032.5,755
+AudioEffectEnvelope      pwm_env2; //xy=1053.75,838.75
+AudioEffectEnvelope      pwm_env4; //xy=1065,1020
+AudioEffectEnvelope      pwm_env3; //xy=1066.25,923.75
+AudioMixer4              lpf_post_mixer;         //xy=1270.75,888.2500305175781
+AudioSynthWaveformDc     dc_main_lpf;            //xy=1326.2501220703125,976
+AudioFilterStateVariable main_lpf;        //xy=1427.0001220703125,883.2500305175781
+AudioOutputI2S           i2s2; //xy=1585.08349609375,881.8611145019531
+AudioConnection          patchCord1(dc1, pwm1);
+AudioConnection          patchCord2(dc2, pwm2);
 AudioConnection          patchCord3(dc3, pwm3);
-AudioConnection          patchCord4(dc2, pwm2);
-AudioConnection          patchCord5(pwm1, pwm_env1);
-//AudioConnection          patchCord6(pwm1, 0, lpf_intermediate_mixer1, 0);
-AudioConnection          patchCord7(pwm_env3, 0, lpf_intermediate_mixer3, 1);
-AudioConnection          patchCord8(pwm_env4, 0, lpf_intermediate_mixer4, 1);
-//AudioConnection          patchCord9(pwm4, 0, lpf_intermediate_mixer4, 0);
-AudioConnection          patchCord10(pwm4, pwm_env4);
-//AudioConnection          patchCord11(pwm2, 0, lpf_intermediate_mixer2, 0);
-AudioConnection          patchCord12(pwm2, pwm_env2);
-AudioConnection          patchCord13(pwm_env1, 0, lpf_intermediate_mixer1, 1);
-//AudioConnection          patchCord14(pwm3, 0, lpf_intermediate_mixer3, 0);
-AudioConnection          patchCord15(pwm3, pwm_env3);
-AudioConnection          patchCord16(pwm_env2, 0, lpf_intermediate_mixer2, 1);
-AudioConnection          patchCord17(lpf_intermediate_mixer1, 0, lpf_post_mixer, 0);
-AudioConnection          patchCord18(lpf_intermediate_mixer2, 0, lpf_post_mixer, 1);
-AudioConnection          patchCord19(lpf_intermediate_mixer3, 0, lpf_post_mixer, 2);
-AudioConnection          patchCord20(lpf_intermediate_mixer4, 0, lpf_post_mixer, 3);
-AudioConnection          patchCord21(lpf_post_mixer, amp_envelope1);
-AudioConnection          patchCord22(lpf_post_mixer, 0, post_amp_env_mixer1, 1);
-AudioConnection          patchCord23(amp_envelope1, 0, post_amp_env_mixer1, 0);
-AudioConnection          patchCord24(post_amp_env_mixer1, 0, main_lpf, 0);
-AudioConnection          patchCord25(post_amp_env_mixer1, envelope2);
-AudioConnection          patchCord26(envelope2, 0, main_lpf, 1);
-AudioConnection          patchCord27(main_lpf, 0, i2s2, 0);
-AudioConnection          patchCord28(main_lpf, 0, i2s2, 1);
+AudioConnection          patchCord4(dc4, pwm4);
+AudioConnection          patchCord5(pwm4, 0, lpf4, 0);
+AudioConnection          patchCord6(pwm4, 0, lpf_intermediate_mixer4, 0);
+AudioConnection          patchCord7(pwm2, 0, lpf2, 0);
+AudioConnection          patchCord8(pwm2, 0, lpf_intermediate_mixer2, 0);
+AudioConnection          patchCord9(pwm1, 0, lpf1, 0);
+AudioConnection          patchCord10(pwm1, 0, lpf_intermediate_mixer1, 0);
+AudioConnection          patchCord11(pwm3, 0, lpf3, 0);
+AudioConnection          patchCord12(pwm3, 0, lpf_intermediate_mixer3, 0);
+AudioConnection          patchCord13(dc_lpf1, 0, lpf1, 1);
+AudioConnection          patchCord14(dc_lpf2, 0, lpf2, 1);
+AudioConnection          patchCord15(dc_lpf3, 0, lpf3, 1);
+AudioConnection          patchCord16(dc_lpf4, 0, lpf4, 1);
+AudioConnection          patchCord17(lpf2, 0, lpf_intermediate_mixer2, 1);
+AudioConnection          patchCord18(lpf4, 0, lpf_intermediate_mixer4, 1);
+AudioConnection          patchCord19(lpf1, 0, lpf_intermediate_mixer1, 1);
+AudioConnection          patchCord20(lpf3, 0, lpf_intermediate_mixer3, 1);
+AudioConnection          patchCord21(lpf_intermediate_mixer2, pwm_env2);
+AudioConnection          patchCord22(lpf_intermediate_mixer3, pwm_env3);
+AudioConnection          patchCord23(lpf_intermediate_mixer1, pwm_env1);
+AudioConnection          patchCord24(lpf_intermediate_mixer4, pwm_env4);
+AudioConnection          patchCord25(pwm_env1, 0, lpf_post_mixer, 0);
+AudioConnection          patchCord26(pwm_env2, 0, lpf_post_mixer, 1);
+AudioConnection          patchCord27(pwm_env4, 0, lpf_post_mixer, 3);
+AudioConnection          patchCord28(pwm_env3, 0, lpf_post_mixer, 2);
+AudioConnection          patchCord29(lpf_post_mixer, 0, main_lpf, 0);
+AudioConnection          patchCord30(dc_main_lpf, 0, main_lpf, 1);
+AudioConnection          patchCord31(main_lpf, 0, i2s2, 0);
+AudioConnection          patchCord32(main_lpf, 0, i2s2, 1);
 AudioControlSGTL5000     sgtl5000_1;
+
+// GUItool: end automatically generated code
